7596d2ce702a2695d6a79b29b3595caa2bafdf71
Josh <joshuaj004@gmail.com>
2016-09-08 09:25:20 -0700

    Auto reload enabled.

--- llama.js
+++ llama.js
@@ -368,7 +368,7 @@ function deleteGame() {
     $("#warning-alert").alert();
     $("#warning-alert").fadeTo(2000, 500).slideUp(500, function() {
         $("#warning-alert").slideUp(500);
-        window.location.reload();
     });
+    window.location.reload();
 }
 //////////////////////////////
